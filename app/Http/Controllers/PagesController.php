3b2fdfcc31fbacdcf0841993d2a94525b768fe9e
kanako-sakai <knksk.526@gmail.com>
2020-07-10 15:57:37 +0000

    About

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //Homeを表示
+    public function getHome(){
+        return view('welcome');
+    }
+    
+    //Aboutを表示
+    public function getAbout(){
+        return view('about');
+    }
+    
+    //プライバシーポリシーを表示
+    public function getPrivacyPolicy(){
+        return view('privacy_policy');
+    }
+}
