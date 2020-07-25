a4d47df97408d6fd314e4d659dbfb80d735143e4
kanako-sakai <knksk.526@gmail.com>
2020-07-25 04:35:56 +0000

    blade updates 3

--- resources/views/users/requesters.blade.php
+++ resources/views/users/requesters.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app_2')
 
 @section('content')
+<section class="ftco-section" id="coaches-section">
+    	<div class="container">
+    		<div class="row justify-content-center pb-5">
+            	<div class="col-md-8 heading-section text-center">
+                	<h2 class="mb-4">マイページ</h2>
+          		</div>
+        	</div>
+            
     <div class="row">
         <aside class="col-sm-4">
             {{-- ユーザ情報 --}}
             @include('users.card')
+            
         </aside>
         <div class="col-sm-8">
             {{-- タブ --}}
@@ -14,8 +23,13 @@
                 <ul class="list-unstyled">
                     @foreach ($requesters as $requester)
                         <li class="media">
-                            {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                            <img class="mr-2 rounded" src="{{ Gravatar::get($requester->sender->email, ['size' => 50]) }}" alt="">
+                            {{-- アイコン --}}
+                            @if(isset($user->picture))
+                            	<img src = "{{ $requester->sender->picture }}" width=50 height=50>
+                            @else
+                            	<img src="{{ Gravatar::get(Gravatar::get($requester->sender->email, ['size' => 50])) }}" alt=""> 
+                            @endif
+                            
                             <div class="media-body">
                                 <div>
                                     {{ $requester->sender->name }}
@@ -28,7 +42,7 @@
                                 <div class="row">
                                 {{-- 承諾・拒否ボダン--}}
                                 @if (Auth::id() != $requester->sender->id)
-                                    @if (Auth::user()->is_matching_from_me($requester->sender->id) || Auth::user()->is_matching_from_others($requester->sender->id))
+                                    @if (Auth::user()->is_matching($requester->sender->id))
                                 
                                         @elseif (Auth::user()->is_requested($requester->sender->id))
                                             {{-- メッセージの表示 --}}
@@ -54,4 +68,5 @@
             @endif
         </div>
     </div>
+</section>    
 @endsection
