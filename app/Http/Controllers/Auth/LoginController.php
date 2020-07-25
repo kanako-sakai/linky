3765647a01358aec91d67f89f6bdad96b22c9dfd
kanako-sakai <knksk.526@gmail.com>
2020-07-25 04:25:36 +0000

    login v2

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    
+    // public function redirectPath()
+    // {
+    //     return redirect()->route('users.show', ['user' => \Auth::id()]);
+    // }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,41 @@
 @extends('layouts.app_2')
 
 @section('content')
-    <section class="hero-wrap hero-wrap-2" style="#" data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-end justify-content-center">
-          <div class="col-md-9 ftco-animate pb-5 text-center">
-            <h1 class="mb-3 bread">ログイン</h1>
-          </div>
-        </div>
-      </div>
-    </section>
+<section class="ftco-section" id="coaches-section">
+    	<div class="container">
+    		<div class="row justify-content-center pb-5">
+            	<div class="col-md-8 heading-section text-center">
+                	<h2 class="mb-4">ログイン</h2>
+                </div>
+            </div>    
     
-    <div class="row">
-        <div class="mb-5 mt-5 col-sm-6 offset-sm-3 pt-5 bg-light">
-
-            {!! Form::open(['route' => 'login.post']) !!}
-                <div class="form-group">
-                    {!! Form::label('email', 'Email') !!}
-                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+            <div class="row">
+                <div class="mb-5 mt-5 col-sm-6 offset-sm-3 pt-5">
+                    
+                    {{--エラーメッセージ--}}
+                    @include('commons.error_messages')
+        
+                    {!! Form::open(['route' => 'login.post']) !!}
+                        <div class="form-group">
+                            {!! Form::label('email', 'メールアドレス') !!}
+                            {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                        </div>
+        
+                        <div class="form-group">
+                            {!! Form::label('password', 'パスワード') !!}
+                            {!! Form::password('password', ['class' => 'form-control']) !!}
+                        </div>
+        
+                        {!! Form::submit('ログイン', ['class' =>'btn py-3 px-4 btn-primary']) !!}
+                    {!! Form::close() !!}
+        
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <p class="mt-2">新規登録がまだの方は {!! link_to_route('signup.get', 'こちら') !!}</p>
+                    {{-- パスワードリセットへのリンク --}}
+                    <p class="mt-2">{!! link_to_route('password.request', 'パスワードをお忘れの方') !!}</p>
+                    <p></p>
                 </div>
-
-                <div class="form-group">
-                    {!! Form::label('password', 'Password') !!}
-                    {!! Form::password('password', ['class' => 'form-control']) !!}
-                </div>
-
-                {!! Form::submit('ログイン', ['class' =>'btn py-3 px-4 btn-primary']) !!}
-            {!! Form::close() !!}
-
-            {{-- ユーザ登録ページへのリンク --}}
-            <p class="mt-2">新規登録がまだの方は {!! link_to_route('signup.get', 'こちら') !!}</p>
+            </div>
         </div>
-    </div>
+</section>    
 @endsection
